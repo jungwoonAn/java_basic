4c34aeab10e9e4b44075c99e88c717f8f2da43b3
jungwoonAn <red0808@naver.com>
2024-08-13 16:52:25 +0900

    2024.08.13-1

---
+++ Java20240813/src/ex05/PrintEx.java
@@ -0,0 +1,12 @@
+package ex05;
+
+public class PrintEx {
+
+	public static void main(String[] args) {
+		System.out.print("kor");
+		System.out.print("kor\n");
+		System.out.println("kor");
+		System.out.println("kor");
+	}
+
+}
