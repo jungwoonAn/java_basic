dcdcf41b3a764c0ef9e825d9d65b484df38e2240
jungwoonAn <red0808@naver.com>
2024-08-13 17:41:44 +0900

    2024.08.13-2

---
+++ Java20240813/src/variable/Var4.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var4 {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		
+		int c,d;  // 여러 변수 한번에 선언할 수 있음.
+	}
+
+}
---
+++ Java20240813/src/variable/ex/VarEx3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VarEx3 {
+
+	public static void main(String[] args) {
+		long bakuk = 10000000000L;
+		System.out.println(bakuk);
+		
+		boolean bool = true;
+		System.out.println(bool);
+	}
+
+}
